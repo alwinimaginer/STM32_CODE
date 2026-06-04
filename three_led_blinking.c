5b67b55fb130178c79088cb2763159336e410eb7
ALWIN NITHISH <168280244+alwinimaginer@users.noreply.github.com>
2026-06-04 21:22:47 +0530

    Update three_led_blinking.c

--- three_led_blinking.c
+++ three_led_blinking.c
@@ -11,7 +11,7 @@
 #define IOPAEN      (1 << 2)
 #define PIN7        (1 << 7)
 #define PIN6        (1 << 6)
-#define PIN5        (1 << 5)
+#define PIN3        (1 << 3)
 
 
 void delay(void)
@@ -24,12 +24,18 @@ int main(void)
     // 1. Enable GPIOA clock
     RCC_APB2ENR |= IOPAEN;
 
-    // 2. Configure PA5 and PA6 as output
+    // 2. Configure PA6, PA7 and PA3 as output
+
+    // Clear bits for PA3 (EXTERNAL)
+        GPIOA_CRL &= ~(0xF << 12);
+
+      // Set PA3 as output push-pull
+        GPIOA_CRL |= (0x2 << 12);
 
     // Clear bits for PA7 (EXTERNAL)
        GPIOA_CRL &= ~(0xF << 28);
 
-       // Set PA6 as output push-pull
+       // Set PA7 as output push-pull
        GPIOA_CRL |= (0x2 << 28);
 
 
@@ -39,30 +45,34 @@ int main(void)
     // Set PA6 as output push-pull
     GPIOA_CRL |= (0x2 << 24);
 
-    // Clear bits for PA5 (LED1)
-     GPIOA_CRL &= ~(0xF << 20);
 
-    // Set PA5 as output push-pull
-    GPIOA_CRL |= (0x2 << 20);
 
     while(1)
     {
 
-     // Toggle PA5(LED1)
-     GPIOA_ODR ^= PIN5;
+     // Toggle PA3(EXTERNAL) D0
+     GPIOA_ODR ^= PIN3;
 
      delay();
 
-     // Toggle PA6(EXTERNAL)
+     GPIOA_ODR ^= PIN3;
+
+
+     // Toggle PA6(EXTERNAL) D12
      GPIOA_ODR ^= PIN6;
 
      delay();
 
+     GPIOA_ODR ^= PIN6;
 
-     // Toggle PA7(EXTERNAL)
+
+     // Toggle PA7(EXTERNAL) D11
      GPIOA_ODR ^= PIN7;
 
      delay();
+
+     GPIOA_ODR ^= PIN7;
+
 
     }
 }
